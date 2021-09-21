65fad022a7807abdd292d573a3f2933992825678
7204-OMER <Eyupbhr24@gmail.com>
2021-09-21 16:52:05 +0300

    add idex.js

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+// import rootReducer from "./redux/index";
+import { Provider } from "react-redux";
+import store from './redux/store/store';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
