5519f5a7f97ab7f92f6ee389ffb8633b0993f129
7204-OMER <Eyupbhr24@gmail.com>
2021-09-21 16:52:33 +0300

    add app.css app.js

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+// import Balance from './components/Balance';
+// import IncomeExpense from './components/IncomeExpense';
+// import TransactionList from './components/TransactionList';
+// import Transactions from './components/Transactions';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>FINANC TRACKER</h1>
+      {/* <Balance/>
+      <IncomeExpense/>
+      <TransactionList/>
+      <Transactions/> */}
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
